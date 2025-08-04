4de6beb747cf8814ba39aa80256abf7dae41da68
Abhishek Kumar <136961510+codingadventure0@users.noreply.github.com>
2025-08-05 00:03:30 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ import "../assets/styles/home.css";
 import { Link } from "react-router-dom";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-import MouseEffect from "../components/MouseEffect";
+// import MouseEffect from "../components/MouseEffect";
 import AchievementsCarousel from "../components/AchievementsCarousel";
 
 // Particle background component
@@ -471,7 +471,7 @@ const Home = () => {
 
   return (
     <div className="home-page">
-      <MouseEffect />
+{/*       <MouseEffect /> */}
       {/* Floating Social Icons */}
       <motion.div
         className="social-icons-container"
